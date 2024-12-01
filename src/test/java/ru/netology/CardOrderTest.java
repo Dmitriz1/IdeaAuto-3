c0fa345a16855cf9aad5c8c7128a8df12b90921c
Dmitriz1 <stud0000264980@study.utmn.ru>
2024-12-01 19:28:53 +0500

    last version2

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -1,17 +1,10 @@
 package ru.netology;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardOrderTest {
-    @BeforeAll
-    public static void setupAll() {
-        WebDriverManager.chromedriver().setup();
-    }
-
     @Test
     public void shouldSubmitFormSuccessfully() {
         open("http://localhost:9999");
