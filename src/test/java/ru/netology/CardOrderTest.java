2145cdffcd0cbe5ec05c41073df3233716ea0a13
Dmitriz1 <stud0000264980@study.utmn.ru>
2024-12-03 09:40:13 +0500

    pre-last version

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -1,18 +1,31 @@
 package ru.netology;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+
 import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CardOrderTest {
+    @BeforeAll
+    public static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
     @Test
-    public void shouldSubmitFormSuccessfully() {
+    public void testOrderForm() {
+        // Открытие страницы приложения
         open("http://localhost:9999");
 
-        $("#name").setValue("Иван Иванов");
-        $("#phone").setValue("+79161234567");
-        $("#agreement").click();
+        $("input[name='name']").setValue("Иван Иванов");
+        $("input[name='phone']").setValue("+71231231234");
+        $("[data-test-id='agreement']").click();
+        $(By.className("button")).click();
 
-        $("#submit").click();
+        String text = $(By.className("alert-success")).getText();
+        assertEquals("Ваша заявка успешно отправлена! " +
+                "Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 }
